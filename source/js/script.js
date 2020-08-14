060c2dc03e86c6af8041fa6fe1aa402b8a4fe339
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-08-14 03:41:17 +0300

    Стилизация блока с фильтром стран на странице "catalog.html"  всех версий

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,19 @@
 
 var pageHeader = document.querySelector(".page-header");
 var pageHeaderButton = document.querySelector(".page-header__button");
-var businessRatesShow = document.querySelector(".profile-list__show");
+var businessRatesShow = document.querySelector(".add-profile__show");
 var businessRates = document.querySelector(".business-rates");
 var businessRatesClose = document.querySelector(".business-rates__close");
+
+var filter = document.querySelector(".filter");
+var filterExpand = document.querySelector(".filter-header__expand");
+var filterCollapse = document.querySelector(".filter-header__collapse");
+var filterCloseButton = document.querySelector(".filter__close-button");
+
+var filterLetter = document.querySelectorAll(".filter__letter");
+var filterCountries = document.querySelectorAll(".filter__countries");
+var filterLetters = document.querySelector(".filter__letters");
+
 
 pageHeader.classList.remove("page-header--no-js");
 
@@ -27,3 +37,34 @@ if (businessRatesClose) {
     businessRates.classList.add("hidden");
   });
 }
+
+if (filterExpand) {
+  filterExpand.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    filter.classList.add("filter--expand");
+  });
+}
+
+if (filterCollapse) {
+  filterCollapse.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    filter.classList.remove("filter--expand");
+  });
+}
+
+if (filterCloseButton) {
+  filterCloseButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    filter.classList.remove("filter--expand");
+  });
+}
+
+
+/* if (filterLetters) {
+  filterLetters.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains("filter__letter")) {
+      filterCountries[filterLetter[evt.target]].classList.toggle("hidden");
+    };
+  });
+} */
